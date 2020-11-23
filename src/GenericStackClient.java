0a647dc730a4105cba677f37db6bce6f7f865347
= <=>
2020-11-23 16:17:43 +0700

    done

---
+++ src/GenericStackClient.java
@@ -0,0 +1,9 @@
+public class GenericStackClient {
+    public static void main(String[] args) {
+        System.out.println("1. Stack of string");
+        MyGenericStack.stackOfString();
+
+        System.out.println("2. Stack of integer");
+        MyGenericStack.stackOfInteger();
+    }
+}
